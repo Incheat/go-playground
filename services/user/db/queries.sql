03a477071984220ae5dba768c2d4c0529244824b
Incheat <incheatwork@gmail.com>
2025-12-13 16:14:18 +0900

    feat(user): add sqlc initialization

---
+++ services/user/db/queries.sql
@@ -0,0 +1,13 @@
+-- name: CreateUser :execresult
+INSERT INTO users (id, email, password_hash)
+VALUES (?, ?, ?);
+
+-- name: GetUserByEmail :one
+SELECT id, email, password_hash, created_at
+FROM users
+WHERE email = ?;
+
+-- name: ListUsers :many
+SELECT id, email, password_hash, created_at
+FROM users
+ORDER BY id;
